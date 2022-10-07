cd23414e58d4c32529fc8dd64d33daaaada697fc
Jian An, Lin <76777822+jianan1104@users.noreply.github.com>
2022-10-07 22:31:54 +0800

    Update _app.tsx
    Add google analysis script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return 
+    <>
+        <Script strategy="lazyOnload">
+                  {`
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', 'G-L0SLFX17MJ', {
+                      page_path: window.location.pathname,
+                      });
+                  `}
+        </Script>
+        <Component {...pageProps} />
+    </>
+   
+
 }
 
 export default MyApp
