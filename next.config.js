a4f0c4afbf4b1aa2007d7fa523e71734316ab5ca
Kyle Mo <kyle.mo@linecorp.com>
2022-10-07 17:49:28 +0800

    ✨ github actions ci main workflow with build and lint

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
-}
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
