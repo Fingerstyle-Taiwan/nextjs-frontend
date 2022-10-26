76521e7eb53d95f82f343a9ec2df7c4fe817da70
jianan1104 <linjianan1104@gmail.com>
2022-10-26 21:27:48 +0800

    refactor(components/shared/Template/Footer/constants.ts): Add social links

--- components/shared/Template/Footer/constants.ts
+++ components/shared/Template/Footer/constants.ts
@@ -3,19 +3,19 @@ export const socialMediaIcons = [
     path: "/github-icon.png",
     width: 48,
     height: 48,
-    link: "/"
+    link: "https://github.com/Fingerstyle-Taiwan"
   },
   {
     path: "/youtube-icon.png",
     width: 48,
     height: 48,
-    link: "/"
+    link: "https://www.youtube.com/channel/UCsWj1Xg6vtfKcorSBDUS4Kw"
   },
   {
     path: "/instagram-icon.png",
     width: 48,
     height: 48,
-    link: "/"
+    link: "https://www.instagram.com/fingerstyle.tw/"
   },
   {
     path: "/facebook-icon2.png",
