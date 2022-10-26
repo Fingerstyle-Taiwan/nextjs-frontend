6880f17fa78806af9f0a9092d7733fc94afe6764
Danny Chen <dannyisadog10@gmail.com>
2022-10-26 10:31:50 +0800

    feat(artist): add artist page entrypoint

---
+++ pages/artist.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Template from 'components/shared/Template'
+import Home from 'components/Home'
+
+const ArtistPage: NextPage = () => {
+  return (
+    <Template>
+      <Home />
+    </Template>
+  )
+}
+
+export default ArtistPage
